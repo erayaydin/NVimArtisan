f4ac514947e38d72c614efd349eb60049f1202b0
Eray Aydın <erayaydinn@protonmail.com>
2022-06-28 13:10:30 +0300

    :alien: Update dapui new config

--- lua/artisan/plugins/dap.lua
+++ lua/artisan/plugins/dap.lua
@@ -22,24 +22,44 @@ require("dapui").setup({
         edit = "e",
         repl = "r",
     },
-    sidebar = {
-        elements = {
-            {
-                id = "scopes",
-                size = 0.25,
+    layouts = {
+        {
+            elements = {
+                'scopes',
+                'breakpoints',
+                'stacks',
+                'watches',
             },
-            { id = "breakpoints", size = 0.25 },
-            { id = "stacks", size = 0.25 },
-            { id = "watches", size = 00.25 },
+            size = 40,
+            position = 'left',
         },
-        size = 40,
-        position = "left",
+        {
+            elements = {
+                'repl',
+                'console',
+            },
+            size = 10,
+            position = 'bottom',
+        }
     },
-    tray = {
-        elements = { "repl" },
-        size = 10,
-        position = "bottom",
-    },
+--    sidebar = {
+--        elements = {
+--            {
+--                id = "scopes",
+--                size = 0.25,
+--            },
+--            { id = "breakpoints", size = 0.25 },
+--            { id = "stacks", size = 0.25 },
+--            { id = "watches", size = 00.25 },
+--        },
+--        size = 40,
+--        position = "left",
+--    },
+--    tray = {
+--        elements = { "repl" },
+--        size = 10,
+--        position = "bottom",
+--    },
     floating = {
         max_height = nil,
         max_width = nil,
