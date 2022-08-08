370ef81ab6627490e13828c05b141f315c418653
Eray Aydın <erayaydinn@protonmail.com>
2022-08-08 15:03:19 +0300

    feat: add `vim-dadbod` and `vim-dadbod-ui`
    Adds `vim-dadbod` and `vim-dadbod-ui` plugins for interacting with
    databases.

--- lua/artisan/plugins/init.lua
+++ lua/artisan/plugins/init.lua
@@ -389,6 +389,19 @@ local plugins = {
 
     -- }}}
 
+    -- Database {{{
+
+    ["tpope/vim-dadbod"] = {},
+
+    ["kristijanhusak/vim-dadbod-ui"] = {
+        config = function()
+            vim.g.db_ui_env_variable_url = 'DATABASE_URL'
+            vim.g.db_ui_env_variable_name = 'DATABASE_NAME'
+        end,
+    },
+
+    -- }}}
+
     -- Tools {{{
 
     ["NTBBloodbath/rest.nvim"] = {
