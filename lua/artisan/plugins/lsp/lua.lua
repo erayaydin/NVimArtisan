820e5b8e782d8e49d7397742380285d25ce90d4b
Eray Aydın <erayaydinn@protonmail.com>
2023-02-22 17:59:44 +0300

    feat: change deprecated lua language server name

--- lua/artisan/plugins/lsp/lua.lua
+++ lua/artisan/plugins/lsp/lua.lua
@@ -5,11 +5,15 @@ return function(lspconfig, before, capabilities)
         print("Lua attached!")
     end
 
-    lspconfig.sumneko_lua.setup({
+    lspconfig.lua_ls.setup({
         on_attach = on_attach,
         capabilities = capabilities,
         settings = {
             Lua = {
+                runtime = {
+                    -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                    version = 'LuaJIT',
+                },
                 diagnostics = {
                     globals = { "vim" },
                 },
