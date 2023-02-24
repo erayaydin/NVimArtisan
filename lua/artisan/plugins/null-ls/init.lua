14cbb0f3e358f5a1bbe79f4b0bfa7840fe797269
Eray Aydın <erayaydinn@protonmail.com>
2023-02-24 17:22:27 +0300

    feat: check executable before register to null-ls

--- lua/artisan/plugins/null-ls/init.lua
+++ lua/artisan/plugins/null-ls/init.lua
@@ -4,29 +4,65 @@ if not installed then
     return
 end
 
+local function get_sources(sources)
+    local tbl = {}
+
+    for source_type, languages in pairs(sources) do
+        for _, programs in pairs(languages) do
+            for _, program in pairs(programs) do
+                -- FIXME: dirty
+                local builtin = ""
+                local with = {}
+                if type(program) == "string" then
+                    builtin = program
+                elseif type(program) == "table" then
+                    builtin = program[1]
+                    table.remove(program, 1)
+                    with = program
+                end
+
+                local configured_builtin = null_ls.builtins[source_type][builtin].with(with)
+
+                if vim.fn.executable(configured_builtin._opts.command) == 1 then
+                    table.insert(tbl, configured_builtin)
+                end
+            end
+        end
+    end
+
+    return tbl
+end
+
 null_ls.setup({
     log_level = "debug",
-    sources = {
-        -- = Formatting =
-
-        -- PHP
-        null_ls.builtins.formatting.phpcsfixer,
-        null_ls.builtins.formatting.phpcbf,
-        null_ls.builtins.formatting.pint,
-
-        -- Scala
-        null_ls.builtins.formatting.scalafmt,
-
-        -- = Diagnostics =
-
-        -- Lua
-        null_ls.builtins.diagnostics.luacheck.with({
-            extra_args = { "--globals vim" },
-        }),
-
-        -- PHP
-        null_ls.builtins.diagnostics.phpcs.with({
-            extra_args = { "--standard=PSR12" },
-        }),
-    },
+    sources = get_sources({
+        diagnostics = {
+            lua = {
+                {
+                    "luacheck",
+                    extra_args = { "--globals vim" },
+                },
+            },
+            php = {
+                {
+                    "phpcs",
+                    extra_args = { "--standard=PSR12" },
+                },
+            },
+            python = {
+                "flake8",
+            }
+        },
+        formatting = {
+            php = {
+                "phpcsfixer", "phpcbf", "pint",
+            },
+            scala = {
+                "scalafmt",
+            },
+            python = {
+                "black",
+            },
+        },
+    }),
 })
