5849a96949833613ef56bb4ed5875ae425177ba6
Eray Aydın <erayaydinn@protonmail.com>
2023-04-18 23:59:29 +0300

    feat: open nvim-tree only for directories

--- lua/artisan/plugins/nvim-tree/init.lua
+++ lua/artisan/plugins/nvim-tree/init.lua
@@ -155,14 +155,10 @@ nvim_tree.setup({
 })
 
 local function open_nvim_tree(data)
-
-  -- buffer is a [No Name]
-  local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
-
   -- buffer is a directory
   local directory = vim.fn.isdirectory(data.file) == 1
 
-  if not no_name and not directory then
+  if not directory then
     return
   end
 
