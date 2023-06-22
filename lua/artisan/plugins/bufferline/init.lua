fa969be4f7ff442b59b62c9656ebf34de3f23157
Eray Aydın <erayaydinn@protonmail.com>
2023-06-22 21:16:30 +0300

    feat: change bufferline diagnostic error icon

--- lua/artisan/plugins/bufferline/init.lua
+++ lua/artisan/plugins/bufferline/init.lua
@@ -53,13 +53,13 @@ bufferline.setup({
         },
         diagnostics = {
             -- you can use a list
-            { enabled = true, icon = 'ﬀ' }, -- ERROR
+            { enabled = true, icon = '' }, -- ERROR
             { enabled = false }, -- WARN
             { enabled = false }, -- INFO
             { enabled = true }, -- HINT
 
             -- OR `vim.diagnostic.severity`
-            [vim.diagnostic.severity.ERROR] = { enabled = true, icon = 'ﬀ' },
+            [vim.diagnostic.severity.ERROR] = { enabled = true, icon = '' },
             [vim.diagnostic.severity.WARN] = { enabled = false },
             [vim.diagnostic.severity.INFO] = { enabled = false },
             [vim.diagnostic.severity.HINT] = { enabled = true },
