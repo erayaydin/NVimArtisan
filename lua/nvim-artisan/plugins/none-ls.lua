45bac1ffa0e4b69fef9a3418c9143a6f82c371c4
Eray Aydın <erayaydinn@protonmail.com>
2024-03-25 22:37:29 +0300

    feat: add diagnostic none-lsp builtins

--- lua/nvim-artisan/plugins/none-ls.lua
+++ lua/nvim-artisan/plugins/none-ls.lua
@@ -12,10 +12,31 @@ local gitsigns = null_ls.builtins.code_actions.gitsigns.with({
   },
 })
 
+local alex = null_ls.builtins.diagnostics.alex.with({
+  command = "alexjs",
+})
+
+local sqlfluff = null_ls.builtins.diagnostics.sqlfluff.with({
+  extra_args = { "--dialect", "postgres" },
+})
+
 null_ls.setup({
   sources = {
     -- Code Actions
     null_ls.builtins.code_actions.gitrebase,
     gitsigns,
+    -- Diagnostics
+    null_ls.builtins.diagnostics.actionlint,
+    alex,
+    null_ls.builtins.diagnostics.ansiblelint,
+    null_ls.builtins.diagnostics.cmake_lint,
+    null_ls.builtins.diagnostics.cppcheck,
+    null_ls.builtins.diagnostics.dotenv_linter,
+    null_ls.builtins.diagnostics.editorconfig_checker,
+    null_ls.builtins.diagnostics.phpcs,
+    null_ls.builtins.diagnostics.phpmd,
+    null_ls.builtins.diagnostics.phpstan,
+    null_ls.builtins.diagnostics.protolint,
+    sqlfluff,
   },
 })
