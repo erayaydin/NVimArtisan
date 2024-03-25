3d4c2cae7a901c07e183151f56667646d4139cd7
Eray Aydın <erayaydinn@protonmail.com>
2024-03-25 19:29:42 +0300

    feat: use experimental module loader instead impatient

--- init.lua
+++ init.lua
@@ -1,15 +1,15 @@
--- Start impatient ASAP
-pcall(require, "impatient")
+-- Enable experimental lua module loader to speed up
+vim.loader.enable()
 
 -- Check nvim version
-if vim.fn.has('nvim-0.9') ~= 1 then
-    error('Need NeoVim v0.9+')
+if vim.fn.has("nvim-0.9") ~= 1 then
+  error("Need NeoVim v0.9+")
 end
 
 -- Check NVimArtisan installation
-local ok, artisan = pcall(require, 'artisan')
+local ok, artisan = pcall(require, "artisan")
 if not ok then
-    error(('Error loading NVimArtisan...\n\n%s'):format(artisan))
+  error(("Error loading NVimArtisan...\n\n%s"):format(artisan))
 end
 
 -- Start NVimArtisan
