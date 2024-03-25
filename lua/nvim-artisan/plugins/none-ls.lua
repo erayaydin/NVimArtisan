6f0dbb85f57591dfae11836c99760a79d7d4e733
Eray Aydın <erayaydinn@protonmail.com>
2024-03-25 21:13:37 +0300

    feat: filter blame action on gitsigns code actions

--- lua/nvim-artisan/plugins/none-ls.lua
+++ lua/nvim-artisan/plugins/none-ls.lua
@@ -4,10 +4,18 @@ if not installed then
   return
 end
 
+local gitsigns = null_ls.builtins.code_actions.gitsigns.with({
+  config = {
+    filter_actions = function(title)
+      return title:lower():match("blame") == nil
+    end,
+  },
+})
+
 null_ls.setup({
   sources = {
     -- Code Actions
     null_ls.builtins.code_actions.gitrebase,
-    null_ls.builtins.code_actions.gitsigns,
+    gitsigns,
   },
 })
